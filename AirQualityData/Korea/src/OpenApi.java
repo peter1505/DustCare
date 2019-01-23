e627fe804e06ec7a64c7748bde939cf66bc9c9ed
peter1505 <peter1505@naver.com>
2019-01-23 02:08:19 -0600

    real 서비스키
    서비스키

--- AirQualityData/Korea/src/OpenApi.java
+++ AirQualityData/Korea/src/OpenApi.java
@@ -9,7 +9,7 @@ import java.net.URL;
             try{
                 String urlstr = "http://openapi.airkorea.or.kr/"
                         + "openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty"
-                        + "?stationName=수내동&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=&ver=1.3";
+                        + "?stationName=수내동&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=서비스키&ver=1.3";
                 URL url = new URL(urlstr);
                 HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
                 urlconnection.setRequestMethod("GET");
